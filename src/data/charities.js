cf16bc1a261195270b0b89d404e83b593294bd14
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2020-08-14 14:49:23 +0700

    Convert HTML ke Svelte file

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('20 August 2020'),
+            profile_photo: 'https://i.pravatar.cc/300',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
