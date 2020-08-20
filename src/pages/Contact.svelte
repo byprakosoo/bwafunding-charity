7ffffb827935c5d774afafc7f62233cb5b898d30
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2020-08-20 15:17:35 +0700

    Finishing

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -5,25 +5,7 @@
 
     export let ready;
     console.log(ready)
-
-//     google.maps.event.addDomListener(window, 'load', init);
-//     function init() {
-	
-
-// 	// Get the HTML DOM element that will contain your map 
-// 	// We are using a div with id="map" seen below in the <body>
-// 	var mapElement = document.getElementById('xs-map');
-
-// 	// Create the Google Map using our element and options defined above
-// 	var map = new google.maps.Map(mapElement, mapOptions);
-
-// 	// Let's also add a marker while we're at it
-// 	var marker = new google.maps.Marker({
-// 		position: new google.maps.LatLng(40.6700, -73.9400),
-// 		map: map,
-// 		title: 'Snazzy!'
-// 	});
-// }
+    
 </script>
 
 <svelte:head>
